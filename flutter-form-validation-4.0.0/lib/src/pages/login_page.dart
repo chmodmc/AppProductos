868b61499ef447dd3d4001e477ccd9a655622a06
chmodmc <64823695+chmodmc@users.noreply.github.com>
2021-03-02 11:32:33 -0600

    Update login_page.dart

--- flutter-form-validation-4.0.0/lib/src/pages/login_page.dart
+++ flutter-form-validation-4.0.0/lib/src/pages/login_page.dart
@@ -217,7 +217,7 @@ class LoginPage extends StatelessWidget {
             children: <Widget>[
               Icon( Icons.person_pin_circle, color: Colors.white, size: 100.0 ),
               SizedBox( height: 10.0, width: double.infinity ),
-              Text('Fernando Herrera', style: TextStyle( color: Colors.white, fontSize: 25.0 ))
+              Text('Inicio', style: TextStyle( color: Colors.white, fontSize: 25.0 ))
             ],
           ),
         )
